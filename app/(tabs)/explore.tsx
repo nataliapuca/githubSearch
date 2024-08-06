99ac1df4ca3ae29fa93fe6ae027eab415d0b7820
natalia_puca <porczynska.natalia@gmail.com>
2024-08-06 11:22:45 +0200

    code reformatting

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -23,14 +23,14 @@ export default function TabTwoScreen() {
       </ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{' '}
-          and{' '}
+          This app has two screens:
+          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>
+          and
           <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
         </ThemedText>
         <ThemedText>
-          The layout file in{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
+          The layout file in
+          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>
           sets up the tab navigator.
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
@@ -40,14 +40,14 @@ export default function TabTwoScreen() {
       <Collapsible title="Android, iOS, and web support">
         <ThemedText>
           You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{' '}
+          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>
           in the terminal running this project.
         </ThemedText>
       </Collapsible>
       <Collapsible title="Images">
         <ThemedText>
-          For static images, you can use the{' '}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
+          For static images, you can use the
+          <ThemedText type="defaultSemiBold">@2x</ThemedText> and
           <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
           provide files for different screen densities
         </ThemedText>
@@ -61,8 +61,8 @@ export default function TabTwoScreen() {
       </Collapsible>
       <Collapsible title="Custom fonts">
         <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{' '}
-          to see how to load{' '}
+          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>
+          to see how to load
           <ThemedText style={{ fontFamily: 'SpaceMono' }}>
             custom fonts such as this one.
           </ThemedText>
@@ -73,7 +73,7 @@ export default function TabTwoScreen() {
       </Collapsible>
       <Collapsible title="Light and dark mode components">
         <ThemedText>
-          This template has light and dark mode support. The{' '}
+          This template has light and dark mode support. The
           <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
           lets you inspect what the user's current color scheme is, and so you
           can adjust UI colors accordingly.
@@ -84,23 +84,23 @@ export default function TabTwoScreen() {
       </Collapsible>
       <Collapsible title="Animations">
         <ThemedText>
-          This template includes an example of an animated component. The{' '}
+          This template includes an example of an animated component. The
           <ThemedText type="defaultSemiBold">
             components/HelloWave.tsx
-          </ThemedText>{' '}
-          component uses the powerful{' '}
+          </ThemedText>
+          component uses the powerful
           <ThemedText type="defaultSemiBold">
             react-native-reanimated
-          </ThemedText>{' '}
+          </ThemedText>
           library to create a waving hand animation.
         </ThemedText>
         {Platform.select({
           ios: (
             <ThemedText>
-              The{' '}
+              The
               <ThemedText type="defaultSemiBold">
                 components/ParallaxScrollView.tsx
-              </ThemedText>{' '}
+              </ThemedText>
               component provides a parallax effect for the header image.
             </ThemedText>
           ),
